50e74b97ae1774955d474b3a9b47071f2063fb8b
Amir Ali Hemmati <amir.negative21@gmail.com>
2023-08-04 18:53:37 +0330

    Update

---
+++ src/crawler/time-ir-crawler.ts
@@ -0,0 +1,36 @@
+import {CheerioCrawler} from "crawlee";
+import {replacePersianNumbers} from "../helper.js";
+import {monthsByName} from "../variables/index.js";
+import moment from "jalali-moment";
+import redisClient from '../redis.js';
+
+export const crawler = new CheerioCrawler({
+    async requestHandler({$, log}) {
+        let currentMonth = replacePersianNumbers($('span.jalali.selectMonth').text());
+        let currentYear = replacePersianNumbers($('span.jalali.selectYear').text());
+
+        log.info(`Crawling month: ${currentMonth} ${currentYear}`);
+
+        const eventsLi = $('.eventsCurrentMonthWrapper ul li');
+        eventsLi.each((_, el) => {
+            const eventSplitter = $(el).text().trimEnd().trimStart().trim().split('\r\n');
+            const dateRaw = eventSplitter?.[0];
+            if (dateRaw && eventSplitter?.[1]) {
+                let [day, month] = dateRaw.split(' ');
+                day = replacePersianNumbers(day);
+                month = replacePersianNumbers(month);
+                let event = eventSplitter[1];
+                let monthNumber = monthsByName.get(month);
+                let eventMoment = moment.from(`${currentYear}-${monthNumber}-${day}`, 'fa', 'YYYY-M-D');
+
+
+                let e = {
+                    date: eventMoment.format('jYYYY/jMM/jDD'),
+                    event: event.trim(),
+                };
+
+                redisClient.set(e.date, e.event);
+            }
+        });
+    },
+});
--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,12 @@
-// For more information, see https://crawlee.dev/
-import {CheerioCrawler, Dataset, purgeDefaultStorages, RequestQueue} from 'crawlee';
-import moment from 'jalali-moment';
-import {replacePersianNumbers} from "./helper.js";
-import {monthsByName, monthsByNumber} from "./variables/index.js";
-import {EventInterface} from "./io/event.i";
-import * as fs from "fs";
+import {monthsByNumber} from "./variables/index.js";
+import {crawler} from "./crawler/time-ir-crawler.js";
+import moment from "jalali-moment";
+import {purgeDefaultStorages} from "crawlee";
+import redisClient from "./redis.js";
 
-
-let events: EventInterface[] = [];
-
-
-const crawler = new CheerioCrawler({
-    async requestHandler({$, log}) {
-        let currentMonth = replacePersianNumbers($('span.jalali.selectMonth').text());
-        let currentYear = replacePersianNumbers($('span.jalali.selectYear').text());
-
-        log.info(`Crawling month: ${currentMonth} ${currentYear}`);
-
-        const eventsLi = $('.eventsCurrentMonthWrapper ul li');
-        eventsLi.each((_, el) => {
-            const eventSplitter = $(el).text().trimEnd().trimStart().trim().split('\r\n');
-            const dateRaw = eventSplitter?.[0];
-            if (dateRaw && eventSplitter?.[1]) {
-                let [day, month] = dateRaw.split(' ');
-                day = replacePersianNumbers(day);
-                month = replacePersianNumbers(month);
-                let event = eventSplitter[1];
-                let monthNumber = monthsByName.get(month);
-                let eventMoment = moment.from(`${currentYear}-${monthNumber}-${day}`, 'fa', 'YYYY-M-D');
-                events.push({
-                    date: eventMoment.format('jYYYY/jMM/jDD'),
-                    event: event.trim(),
-                });
-            }
-        });
-    },
-});
-
+await purgeDefaultStorages();
+await redisClient.connect();
 const now = moment();
-
 monthsByNumber.forEach(async (_, key) => {
     await crawler.addRequests([
         {
@@ -55,7 +23,4 @@ monthsByNumber.forEach(async (_, key) => {
 })
 
 await crawler.run();
-await purgeDefaultStorages();
-fs.writeFile('./data.json', JSON.stringify(events), () => {
-    console.log('ok')
-});
+await redisClient.disconnect();
